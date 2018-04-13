7f598dd049f55ed298a9cef15447578ffa73d2ce
Joyce Chun <wooyangee@gmail.com>
2018-04-13 03:28:45 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -70,8 +70,8 @@ class TicTacToe
   return count
   end
   
-  def current_player(board)
-    if turn_count(board) % 2 == 0
+  def current_player
+    if turn_count % 2 == 0
       return "X"
     else
       return "O"
